3f45ed4b6da18362c3a0e06273ef06d81e3ecdf0
mendesjorge <luis.7win@gmail.com>
2014-01-28 02:36:11 +0000

    [IFMISSING]

--- controllers/default.js
+++ controllers/default.js
@@ -1,7 +1,10 @@
 exports.install = function(framework) {
         framework.route('/', view_homepage);
         framework.route('/contactos',view_contacts);
-        framework.route('/vendas',view_sells)
+        framework.route('/vendas',view_sells);
+        framework.route('/assistencia',view_assist);
+        framework.route('/aluguer',view_rent);
+        framework.route('/pecas',view_parts);
         framework.route('#404', view_404);
 
 };
@@ -16,6 +19,16 @@ function view_contacts(){
 
 function view_sells(){
         this.view('vendas');
+}
+
+function view_assist(){
+        this.view('assistencia');
+}
+function view_rent(){
+        this.view('aluguer');
+}
+function view_parts(){
+        this.view('pecas');
 }
 
 function view_404(){
