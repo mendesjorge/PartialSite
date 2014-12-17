34c40169355b1a1680b4e7a45fd2eae23dc4f76a
mendesjorge <luis.7win@gmail.com>
2014-12-17 01:02:22 +0000

    changes to heroku deploy

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var framework = require('total.js');
+var framework = require('total.js').http('release');
 var http = require('http');
 var fs = require('fs');
 
@@ -39,4 +39,4 @@ framework.onAuthorization = function(req,res,flags,next){
 
 
 framework.run(http, debug, port);
-console.log('http://{0}:{1}/'.format(framework.ip, framework.port));
+console.log('http://{0}:{1}/',framework.ip, framework.port);
